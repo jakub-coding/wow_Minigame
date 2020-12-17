b010508ebda1ee8baefc531a49cca6836d4843a9
jakub-coding <ljakubsky@jakub-coding.de>
2020-12-17 17:56:25 +0100

    create game components: header, table, highsore

---
+++ src/components/gameHighscore.vue
@@ -0,0 +1,9 @@
+<template> </template>
+
+<script>
+export default {
+  name: "gameHighscore"
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/gameTable.vue
@@ -0,0 +1,9 @@
+<template> </template>
+
+<script>
+export default {
+  name: "gameTable"
+};
+</script>
+
+<style scoped></style>
