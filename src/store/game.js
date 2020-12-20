3771021dba5afdfff95126972e9abd59aacdaebc
jakub-coding <ljakubsky@jakub-coding.de>
2020-12-20 10:41:07 +0100

    creting store modul with game data

---
+++ src/store/game.js
@@ -0,0 +1,7 @@
+export default {
+  state: {
+    gameLevel: 0,
+    gameRound: 0,
+    gameRoundTimer: 5000
+  }
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
 import player from "@/store/player";
 import cards from "@/store/cards";
+import game from "@/store/game";
 
 export default createStore({
   modules: {
     player,
-    cards
+    cards,
+    game
   }
 });
